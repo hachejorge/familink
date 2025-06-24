f9ff5bf66cefd2dd7e6b1301b086a855f410ab44
hachejorge <872838@unizar.es>
2025-06-24 21:24:42 +0200

    neuvo commit

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,19 @@
-
-  // src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptor } from './app/services/token.interceptor';
-
-import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { TokenInterceptor } from './app/services/token.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(HttpClientModule), 
+    importProvidersFrom(HttpClientModule),
     provideAnimations(),
-    provideRouter(routes), 
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }  // 👈 importante
+    provideRouter(routes),
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
   ]
-}).catch(err => console.error(err));
+}).catch((err: unknown) => console.error(err));
